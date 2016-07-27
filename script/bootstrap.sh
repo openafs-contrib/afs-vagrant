f35d37fe97efb886e218630929633c502f55e644
Joe Gorse <jhgorse@gmail.com>
2016-07-26 23:45:49 -0400

    Use a subshell to keep the directory.

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -18,6 +18,17 @@ fi
 
 # vagrant up
 vagrant up
-echo "Don't forget to "
-echo "${bold}cd $(PWD)${normal}"
-echo "  since shells ${bold}do not${normal} readily inherent PWD from their children."
+echo "You are in a subshell for $1 via the bootstrap script."
+echo "To keep things clean, ${bold}vagrant destroy${normal} when done and delete the directory."
+echo "Or you may keep this vagrant box with ${bold}vagrant {halt|reload|up}${normal} and returning "
+echo "to this directory as necessary."
+
+# set up prompt to help us remember we're in a subshell, cd to
+# the vagrant target dir and start $SHELL
+shell=$SHELL
+if test "x$SHELL" = "x/bin/bash"
+then
+  # debian/ubuntu resets our PS1.  bastards.
+  shell="$SHELL --noprofile"
+fi
+PS1="[$1] $PS1" $shell
